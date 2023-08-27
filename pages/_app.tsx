fbc042278ac60d232556b4eb4c06fa1fdfe83f50
yudai <sugi.nut@gmail.com>
2023-08-27 22:59:58 +0900

    メインビジュアル追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NGT金融リテラシー向上委員会</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    return (
+        <Html lang="jp">
+            {/* ヘッダーにGoogle Analyticsのスクリプトコードを埋め込み */}
+            {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-VEG33L98SB"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-VEG33L98SB', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                }}
+            /> */}
+
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Head />
+        <body>
+            {children}
+            <Main />
+            <NextScript />
+        </body>
+        </Html>
+    )
+}
