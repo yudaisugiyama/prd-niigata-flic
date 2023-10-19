0c4c5908a3b1ce2261e9dbbdca99e4fdda482d78
yudai <sugi.nut@gmail.com>
2023-10-19 17:44:54 +0200

    modified <a> to <Link>

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className="bg-black p-4">
-      <a href="/" className="text-2xl text-white font-semibold">NGT金融リテラシー向上委員会</a>
+      <Link href="/" className="text-2xl text-white font-semibold">NGT金融リテラシー向上委員会</Link>
       <div className="container mx-auto flex items-center justify-between">
         <nav className="space-x-4">
-          <a href="/" className="text-white hover:text-gray-300">Home</a>
-          <a href="/about" className="text-white hover:text-gray-300">Event</a>
-          <a href="/contact" className="text-white hover:text-gray-300">Blog</a>
-          <a href="/contact" className="text-white hover:text-gray-300">Support</a>
+          <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+          <Link href="/about" className="text-white hover:text-gray-300">Event</Link>
+          <Link href="/contact" className="text-white hover:text-gray-300">Blog</Link>
+          <Link href="/contact" className="text-white hover:text-gray-300">Support</Link>
         </nav>
       </div>
     </header>
