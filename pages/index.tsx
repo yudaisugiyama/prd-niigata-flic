e6f9362fb2c7b1a7fe031e34560e1ffa8796b650
yudai <sugi.nut@gmail.com>
2023-12-15 14:24:04 +0900

    [add] タイムラインのデザイン修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,12 @@ export default function Home({ articles }: Props) {
         onEnded={(e) => restartVideo(e.target as HTMLVideoElement)}>
       </video>
 
-      <div style={{ width: '50%', margin: '30px', float: 'left' }}>
+      {/* タイムライン */}
+      <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 text-2xl font-semibold">
+        Timeline
+      </h1>
+
+      <div className="container px-10 pt-10  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5" style={{height: '600px', overflowY: 'scroll' }}>
         {/* Twitterタイムラインの埋め込みコード */}
         <a className="twitter-timeline" href="https://twitter.com/niigataflic?ref_src=twsrc%5Etfw">Tweets by niigataflic</a> <script async src="https://platform.twitter.com/widgets.js"></script>
       </div>
@@ -58,12 +63,19 @@ export default function Home({ articles }: Props) {
             alt="Sunset in the mountains"
           />
           <div className="px-6 py-4">{article.title}</div>
-          <div className="px-6 pt-4 pb-2">
-            {article.tag && (
-            <span className="inline-block bg-gray-200 bg-opacity-75 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 text-white shadow-md mr-2 mb-2">{article.tag}</span>
-            )}
-          </div>
         </Link>
+        <div className="px-6 pt-4 pb-2">
+          <Link href={`event/${article.id}`}>
+            {article.url && (
+              <span className="inline-block bg-gray-200 bg-opacity-75 hover:bg-gray-600 active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 text-white shadow-md mr-2 mb-2">{article.tag}</span>
+              )}
+          </Link>
+          <Link href={`${article.url}`}>
+            {article.url && (
+              <span className="inline-block bg-sky-500 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 text-white shadow-md m-3 ">参加申込</span>
+              )}
+          </Link>
+        </div>
       </div>
       ))}
       </div>
