53e3e5c9fe2871a9a903de51a7c9c8a22ea491b0
yudai <sugi.nut@gmail.com>
2023-12-18 14:17:37 +0900

    [fix] mx-autoを削除

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className="bg-black p-4">
+    <header className="bg-black p-4 text-left">
       <Link href="/" className="text-2xl text-white font-semibold">NGT金融リテラシー向上委員会</Link>
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container flex items-center justify-between">
         <nav className="space-x-4">
           <Link href="/" className="text-white hover:text-gray-300">Home</Link>
           <Link href="/event" className="text-white hover:text-gray-300">Event</Link>
