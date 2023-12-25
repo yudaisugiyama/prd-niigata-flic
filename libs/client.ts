5e4d06c57cbeaf46e8c8e0e6dab2d69737a6a979
yudai <sugi.nut@gmail.com>
2023-12-25 11:58:34 +0900

    [fix] change apiKey

--- libs/client.ts
+++ libs/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'microcms-js-sdk'
 export const client = createClient({
   serviceDomain: "dev-niigata-flic",
-  apiKey: process.env.API_KEY || '',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
 })
